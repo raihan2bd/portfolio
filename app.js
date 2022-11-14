8f97ecc5e44308db16c99365716b0572e749dc97
raihan2bd <raihan2bd.official@gmail.com>
2022-11-14 14:00:14 +0600

    Driver 1 Add Navigation system in Html and Javascript

---
+++ app.js
@@ -0,0 +1,27 @@
+const mobileMenu = document.querySelector(".header_menu")
+const navItems = document.querySelector(".mobile_nav_items")
+const selectNavItems = document.querySelectorAll('#m_nav')
+const crossIcon = document.querySelector(".toggle-cross")
+
+let setMobileNav = false;
+let activeNavEvent = false;
+mobileMenu.addEventListener('click', toggleNav)
+crossIcon.addEventListener('click', toggleNav)
+selectNavItems.forEach(item => {
+  item.addEventListener('click', toggleNav)
+})
+
+
+
+function toggleNav() {
+  if (setMobileNav) {
+
+    navItems.classList.remove('df')
+    setMobileNav = false
+
+  } else {
+    navItems.classList.add('df')
+    setMobileNav = true
+
+  }
+}
