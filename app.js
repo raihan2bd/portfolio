42f26cb4040c81ec9b376522686fcd2e8fcbc0e3
raihan2bd <raihan2bd.official@gmail.com>
2022-11-16 15:22:19 +0600

    Fix Some linter error from this project

--- app.js
+++ app.js
@@ -268,6 +268,7 @@ function onSubmit(e) {
   const formInfo = document.getElementById('form-info');
   const email = inputEmail.value;
 
+  // Check if email value is lowercase or not
   if (email !== email.toLowerCase()) {
     e.preventDefault();
     inputEmail.classList.add('invalid');
@@ -280,10 +281,12 @@ function onSubmit(e) {
 }
 const contactForm = document.getElementById('contact-form');
 contactForm.addEventListener('submit', onSubmit);
+
+// Remove Error onchange from the form.
 const inputEmail = document.getElementById('email');
 const formInfo = document.getElementById('form-info');
-inputEmail.addEventListener('change', (e) => {
+inputEmail.addEventListener('change', () => {
   inputEmail.classList.remove('invalid');
   formInfo.classList.remove('error');
-  formInfo.innerText = "";
-})
+  formInfo.innerText = '';
+});
